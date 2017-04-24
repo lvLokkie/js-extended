91680c6c1d68c9e1fd6f20665b794422986a28e8
Рязанов Иван <Rakorts@yandex.ru>
2017-04-24 23:12:21 +0500

    fixed routing for gitpages

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ require(
     ],
     function ($, Menu, MenuItem, rgx) {
         var items = [
-           new MenuItem('Main', '/js-extended-cource/index.html'),
-           new MenuItem('Menu creation lesson', '/js-extended-cource/lessons/menu.html'),
-           new MenuItem('Ajax creation lesson', '/js-extended-cource/lessons/ajax.html'),
-           new MenuItem('Tabs creation lesson', '/js-extended-cource/lessons/tabs.html')
+           new MenuItem('Main', 'https://lvlokkie.github.io/js-extended-cource/'),
+           new MenuItem('Menu creation lesson', 'https://lvlokkie.github.io/js-extended-cource/lessons/menu.html'),
+           new MenuItem('Ajax creation lesson', 'https://lvlokkie.github.io/js-extended-cource/lessons/ajax.html'),
+           new MenuItem('Tabs creation lesson', 'https://lvlokkie.github.io/js-extended-cource/tabs.html')
          ],
             menu = new Menu(items),
             $menu = menu.render(),
@@ -30,77 +30,6 @@ require(
         $menuTarget.append($menu);
 
         regexp.printToConsole();
-
-    //     $formTarget = $('#form'),
-    //     $sendButton = $('#send'),
-    //
-    //     constraints = {
-    //         username: {
-    //             presence: true,
-    //             length: {
-    //                 minimum: 6,
-    //                 maximum: 100
-    //             },
-    //             format: {
-    //                 pattern: '[a-z0-9]+',
-    //                 flags: 'i',
-    //                 message: 'can only contain a-z and 0-9'
-    //             }
-    //         },
-    //         password: {
-    //             presence: true,
-    //             length: {
-    //                 minimum: 6,
-    //                 maximum: 100
-    //             }
-    //         },
-    //         email: {
-    //             presence: true,
-    //             email: true
-    //         },
-    //         gender: {
-    //             presence: true,
-    //             inclusion: {
-    //                 within: ['f'],
-    //                 message: '^Sorry, this service is for man only'
-    //             }
-    //         },
-    //         credit_card: {
-    //             presence: true,
-    //             format: {
-    //                 pattern: /^(34|37|4|5[1-5]).*$/,
-    //                 message: function (value, attribute, validatorOptions, attributes, globalOptions) {
-    //                     return validate.format('^%{num} is not a valid credit card number', {
-    //                         num: value
-    //                     });
-    //                 }
-    //             },
-    //             length: function (value, attributes, attributeName, options, constraints) {
-    //                 if (value) {
-    //                     // Amex
-    //                     if ((/^(34|37).*$/).test(value)) return { is: 15 };
-    //
-    //                     // Visa, Mastercard
-    //                     if ((/^(4|5[1-5]).*$/).test(value)) return { is: 16 };
-    //                 }
-    //
-    //                 // Unknown card, don't validate length
-    //                 return false;
-    //             }
-    //         },
-    //         bio: {
-    //             presence: true
-    //         }
-    //     };
-    //
-    // $menuTarget.append($menu);
-    //
-    // validate($formTarget, constraints);
-    //
-    // $sendButton.on('click', function (ev) {
-    //     var errors = validate($formTarget, constraints);
-    //     console.log(errors);
-    // });
 
 
 });
